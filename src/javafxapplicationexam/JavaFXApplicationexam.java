70999e8fdec46635e5cb70cab168a523d8f3b5a0
Nouran ♡ <nouran.ali.khamis@gmail.com>
2026-05-10 22:57:21 +0300

    Update JavaFXApplicationCalculater.java

--- src/javafxapplicationexam/JavaFXApplicationexam.java
+++ src/javafxapplicationexam/JavaFXApplicationexam.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplicationexam;
+package javafxapplicationCalculater;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -22,7 +22,7 @@ import javafx.stage.Stage;
  *
  * @author Nouran
  */
-public class JavaFXApplicationexam extends Application {
+public class JavaFXapplicationCalculater extends Application {
     
     @Override
     public void start(Stage primaryStage) {
